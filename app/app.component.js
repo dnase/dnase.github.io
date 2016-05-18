d1fb315f44701a3d435ee65628e8b5dfe322098f
Drew Nase <drew.nase@coxinc.com>
2016-05-18 17:22:05 -0400

    new message

--- app/app.component.js
+++ app/app.component.js
@@ -15,8 +15,8 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>Hi</h1>'
-        }), 
+            template: '<h1>Coming soon...</h1>'
+        }),
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
