9caf02c7571ad0a400a201b9273740b15d02cb83
Drew Nase <drew.nase@coxinc.com>
2016-06-08 13:42:36 -0400

    angularifying

--- app/app.component.js
+++ app/app.component.js
@@ -15,7 +15,7 @@ var AppComponent = (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: ""
+            template: "\n  <div class=\"row container\" >\n    <div class=\"col s12 valign-wrapper\">\n      <img src=\"images/drewface1.png\" alt=\"\" class=\"circle responsive-img\" style=\"min-width: 90px;\" />\n      <h1 class=\"header inline-pls\">Drew Nase</h1>\n    </div>\n  </div>\n  "
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,14 @@ import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: ``
+  template: `
+  <div class="row container" >
+    <div class="col s12 valign-wrapper">
+      <img src="images/drewface1.png" alt="" class="circle responsive-img" style="min-width: 90px;" />
+      <h1 class="header inline-pls">Drew Nase</h1>
+    </div>
+  </div>
+  `
 })
 
 export class AppComponent { }
