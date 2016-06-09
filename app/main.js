577bcf1e2e3d493ad9e1cc89ae374c6321e8c22b
Drew Nase <drew.nase@coxinc.com>
2016-06-08 22:17:34 -0400

    removed unused components

--- app/main.js
+++ app/main.js
@@ -1,13 +1,9 @@
 "use strict";
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var heading_component_1 = require('./heading.component');
-var wyg_component_1 = require('./wyg.component');
-var description_component_1 = require('./description.component');
 var projects_component_1 = require('./projects.component');
 var logos_component_1 = require('./logos.component');
 platform_browser_dynamic_1.bootstrap(heading_component_1.HeadingComponent);
-platform_browser_dynamic_1.bootstrap(wyg_component_1.WYGComponent);
-platform_browser_dynamic_1.bootstrap(description_component_1.DescriptionComponent);
 platform_browser_dynamic_1.bootstrap(projects_component_1.ProjectsComponent);
 platform_browser_dynamic_1.bootstrap(logos_component_1.LogosComponent);
 //# sourceMappingURL=main.js.map
--- app/main.ts
+++ app/main.ts
@@ -2,16 +2,10 @@ import { bootstrap }    from '@angular/platform-browser-dynamic';
 
 import { HeadingComponent } from './heading.component';
 
-import { WYGComponent } from './wyg.component';
-
-import { DescriptionComponent } from './description.component';
-
 import { ProjectsComponent } from './projects.component';
 
 import { LogosComponent } from './logos.component';
 
 bootstrap(HeadingComponent);
-bootstrap(WYGComponent);
-bootstrap(DescriptionComponent);
 bootstrap(ProjectsComponent);
 bootstrap(LogosComponent);
