f260044832457f26ec0d3cfa3d21621f47e78663
Drew Nase <drew.nase@coxinc.com>
2016-06-09 22:05:52 -0400

    added a link to the source code for the website

--- app/projects.component.js
+++ app/projects.component.js
@@ -29,5 +29,6 @@ var PROJECTS = [
     { name: 'AGPython', description: 'Affine gap alignment of gene sequences in Python', url: 'https://github.com/dnase/affine-gap-sequence-alignment' },
     { name: 'LP5Responsive', description: 'A responsive theme using Bootstrap for Luminis Portal', url: 'https://github.com/dnase/lp5-theme-bootstrap' },
     { name: 'PyCrack', description: 'A Python script for cracking SHA1-encrypted (LDAP) password hashes', url: 'https://github.com/dnase/pythoncrack' },
+    { name: 'drewnase.com', description: 'This website, written in AngularJS with MaterializeCSS', url: 'https://github.com/dnase/dnase.github.io' }
 ];
 //# sourceMappingURL=projects.component.js.map
--- app/projects.component.ts
+++ app/projects.component.ts
@@ -17,4 +17,5 @@ var PROJECTS: Project[] = [
   {name: 'AGPython', description: 'Affine gap alignment of gene sequences in Python', url: 'https://github.com/dnase/affine-gap-sequence-alignment'},
   {name: 'LP5Responsive', description: 'A responsive theme using Bootstrap for Luminis Portal', url: 'https://github.com/dnase/lp5-theme-bootstrap'},
   {name: 'PyCrack', description: 'A Python script for cracking SHA1-encrypted (LDAP) password hashes', url: 'https://github.com/dnase/pythoncrack'},
+  {name: 'drewnase.com', description: 'This website, written in AngularJS with MaterializeCSS', url: 'https://github.com/dnase/dnase.github.io'}
 ]
