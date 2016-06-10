35bee4f6e1867b1071d8b6de068fb6f80ae02903
Drew Nase <drew.nase@coxinc.com>
2016-06-09 22:31:04 -0400

    more cleanup of unused modules

--- app/description.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, AfterViewInit } from '@angular/core';
-
-@Component({
-  selector: 'description',
-  templateUrl: 'templates/description.html'
-})
-
-export class DescriptionComponent { }
