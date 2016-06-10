6adbdf15f908b174e5a1b161c7bdca66937cc9da
Drew Nase <drew.nase@coxinc.com>
2016-06-09 22:29:15 -0400

    removed unused clutter

--- app/wyg.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component, AfterViewInit } from '@angular/core';
-
-@Component({
-  selector: 'what-you-get',
-  templateUrl: 'templates/what-you-get.html'
-})
-
-export class WYGComponent { }
